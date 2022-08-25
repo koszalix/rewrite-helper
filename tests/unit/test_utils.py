5f3a78ab580b79971e2c0be0b6f9552c5e5a5ea5
Pawel Koszalka <koszalix@gmail.com>
2022-08-25 23:11:12 +0200

    test update for utils

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.utils import check_protocol_slashed
+from src.utils import safe_parse_value
 
 class CheckProtocolSlashed(unittest.TestCase):
     def test_http_slashed(self):
@@ -26,6 +27,24 @@ class CheckProtocolSlashed(unittest.TestCase):
 
     def test_https_single_slash(self):
         self.assertEqual(check_protocol_slashed("https:/"), "https://")
+
+
+class SafeParseValue(unittest.TestCase):
+    def test_key_in_content_value_assigned(self):
+        content = {'key-a': 32, 'key-B': 33, 'key-c': 'key-B'}
+        self.assertEqual(safe_parse_value(content=content, key='key-b', default_value=80), 33)
+
+    def test_key_in_content_value_assigned_strings(self):
+        content = {'key-a': '32', 'key-B': '33', 'key-c': 'key-B'}
+        self.assertEqual(safe_parse_value(content=content, key='key-B', default_value=80), '33')
+
+    def test_default_value(self):
+        content = {'key-a': '32', 'key-B': '33', 'key-c': 'key-b'}
+        self.assertEqual(safe_parse_value(content=content, key='key-t', default_value=80), 80)
+
+    def test_default_value_strings(self):
+        content = {'key-a': '32', 'key-B': '33', 'key-c': 'key-B'}
+        self.assertEqual(safe_parse_value(content=content, key='key-t', default_value='80'), '80')
 
 if __name__ == '__main__':
     unittest.main()
