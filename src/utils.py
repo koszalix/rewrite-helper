6ec98c048f92666e2978707b19c366a76f584193
Pawel Koszalka <koszalix@gmail.com>
2022-08-28 12:04:21 +0200

    changed name of safe_parse_value

--- src/utils.py
+++ src/utils.py
@@ -22,23 +22,23 @@ def check_protocol_slashed(proto=""):
         return proto + "://"
 
 
-def safe_parse_value(content, key, default_value):
-        """
-        Try to find a value assigned to key in dictionary, if key wasn't found return default value
-        :param content: dict: a content to search in
-        :param key: str: key to find
-        :param default_value: default value in key wasn't found
-        :return: value assigned to key, or default value
-        """
-        if content is None:
-            return default_value
-        if key in content:
-            if content[key] is not None:
-                return content[key]
-            else:
-                return default_value
+def parse_value_with_default(content, key, default_value):
+    """
+    Try to find a value assigned to key in dictionary, if key wasn't found return default value
+    :param content: dict: a content to search in
+    :param key: str: key to find
+    :param default_value: default value in key wasn't found
+    :return: value assigned to key, or default value
+    """
+    if content is None:
+        return default_value
+    if key in content:
+        if content[key] is not None:
+            return content[key]
         else:
             return default_value
+    else:
+        return default_value
 
 
 def check_linux_permissions(permissions, target):
