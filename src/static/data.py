4dd4619e77729286614f53ee2086724387d0eaec
Pawel Koszalka <koszalix@gmail.com>
2022-08-30 18:38:46 +0200

    reformat of static data

--- src/static/data.py
+++ src/static/data.py
@@ -1,6 +1,8 @@
 """
 Stores default configuration values, if you change any of these values change test case too
 """
+
+
 # api
 class Api:
     proto = "http"
@@ -12,11 +14,13 @@ class Api:
         exit_on_false = False
         retry_after = 10
 
+
 # ping jobs
 class PingJob:
     interval = 60
     count = 2
     timeout = 2
+
 
 # http job
 class HttpJob:
