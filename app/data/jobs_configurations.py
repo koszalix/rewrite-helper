eb977e8bab5932dd08abd93b6496f1b5913045b3
Pawel Koszalka <koszalix@gmail.com>
2022-09-26 21:03:26 +0200

    added stroage class for ping job

--- app/data/jobs_configurations.py
+++ app/data/jobs_configurations.py
@@ -1,14 +1,12 @@
 from typing import Union
 
+
 class JobDescriptor:
     job_id = 0
     job_type = ""
 
 
-class Common:
-    """
-    Variables and methods common for every (or most of) class
-    """
+class IterationEngine:
 
     # number of jobs
     __count = 0
@@ -28,8 +26,21 @@ class Common:
             return True
         return False
 
+    def __iter__(self):
+        self.__index = 0
+        return self
 
-class DNS(Common):
+    def __next__(self):
+        if self.__index >= self.__count:
+            raise StopIteration
+
+        temporary_index = self.__index
+        self.__index += 1
+
+        return temporary_index
+
+
+class DNS(IterationEngine):
     """
     Stores DNS answer and domain, answer[0] is primary answer
     """
@@ -61,26 +72,13 @@ class DNS(Common):
         return self.__answers[idx]
 
 
-class JobHttp(DNS, Common):
+class JobHttp(DNS, IterationEngine):
     """
     Stores configuration for http job
     """
     __interval = []
     __status_code = []
     __proto = []
-
-    def __iter__(self):
-        self.__index = 0
-        return self
-
-    def __next__(self):
-        if self.__index >= self.__count:
-            raise StopIteration
-
-        temporary_index = self.__index
-        self.__index += 1
-
-        return temporary_index
 
     def append(self, interval: int, status_code: int, proto: str, domain: str, answers: list):
         """
@@ -134,4 +132,64 @@ class JobHttp(DNS, Common):
         if self._check_idx(idx=idx) is True:
             return False
         return self.__proto[idx]
-    
+
+
+class JobPing(DNS, IterationEngine):
+    __interval = []
+    __count = []
+    __timeout = []
+    __privileged = []
+
+    def append(self, interval: int, count: int, timeout: int, domain: str, answers: list, privileged : bool):
+        self.__interval.append(interval)
+        self.__count.append(count)
+        self.__timeout.append(timeout)
+        self.__domain.append(domain)
+        self.__answers.append(answers)
+        self.__privileged.append(privileged)
+
+        self.__count += 1
+
+    def get_interval(self, idx: int) -> Union[int, bool]:
+        """
+        Get interval of specific http job
+
+        :param idx: job id
+        :return: job interval or False in case of failure (idx is not valid)
+        """
+        if self._check_idx(idx=idx) is True:
+            return False
+        return self.__interval[idx]
+
+    def get_count(self, idx: int) -> Union[int, bool]:
+        """
+        Get package count of specific http job
+
+        :param idx: job id
+        :return: job interval or False in case of failure (idx is not valid)
+        """
+        if self._check_idx(idx=idx) is True:
+            return False
+        return self.__count[idx]
+
+    def get_timeout(self, idx: int) -> Union[int, bool]:
+        """
+        Get timeout of specific http job
+
+        :param idx: job id
+        :return: job interval or False in case of failure (idx is not valid)
+        """
+        if self._check_idx(idx=idx) is True:
+            return False
+        return self.__timeout[idx]
+
+    def get_privileged(self, idx: int) -> Union[int, bool]:
+        """
+        Get state of privileged run of specific http job
+
+        :param idx: job id
+        :return: job interval or False in case of failure (idx is not valid)
+        """
+        if self._check_idx(idx=idx) is True:
+            return False
+        return self.__privileged[idx]
